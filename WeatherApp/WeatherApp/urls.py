90c6eb031e41655f84e9d8181ec1ffff2bae3df3
bmohama <bmohama@ncsu.edu>
2022-02-21 22:55:15 -0500

    Made default page go to forecast

--- WeatherApp/WeatherApp/urls.py
+++ WeatherApp/WeatherApp/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('forecast/', include('forecast.urls')),
+    path('', include('forecast.urls')),
 ]
